8753a6dd913c713b15f67868c1bf727e86871fb9
Liam <contact@liamlime.com>
2017-04-22 07:09:52 +0200

    - Adds splash screen - Adds main game scene with building - Building can add floors - Floors can spawn tiles - Can drag-drop to move camera around

---
+++ Assets/Code/Building.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+
+    public List<Floor> Floors;
+
+
+    public float FLOOR_HEIGHT = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Floors = new List<Floor>();
+        
+        AddFloor();
+        AddFloor();
+        AddFloor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddFloor()
+    {
+        GameObject go = Instantiate(GameController.obj.PF_Floor);
+        Transform t = go.GetComponent<Transform>();
+        Floor floor = go.GetComponent<Floor>();
+
+        Floors.Add(floor);
+
+        t.SetParent(this.transform);
+        t.localScale = Vector3.one;
+        t.rotation = Quaternion.identity;
+        t.localPosition = new Vector3(0, (Floors.Count - 1) * FLOOR_HEIGHT, 0);
+
+        floor.SetUp(this);
+    }
+}
---
+++ Assets/Code/Floor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Floor : MonoBehaviour
+{
+    public Building Owner;
+
+    public List<Tile> Tiles;
+
+    public int TILES_PER_FLOOR = 40;
+    public float TILE_WIDTH = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetUp(Building building)
+    {
+        Owner = building;
+
+        Tiles = new List<Tile>();
+        for (int i = 0; i < TILES_PER_FLOOR; i++)
+        {
+            Tiles.Add(CreateTile(i));
+        }
+    }
+
+    public Tile CreateTile(int x)
+    {
+        GameObject go = Instantiate(GameController.obj.PF_Tile);
+        Transform t = go.GetComponent<Transform>();
+        Tile tile = go.GetComponent<Tile>();
+
+        t.SetParent(this.transform);
+        t.localScale = Vector3.one;
+        t.rotation = Quaternion.identity;
+        t.transform.localPosition = new Vector3(((-TILES_PER_FLOOR / 2) + x) * TILE_WIDTH, 0, 0);
+
+        tile.SetUp(this);
+
+        return tile;
+    }
+}
